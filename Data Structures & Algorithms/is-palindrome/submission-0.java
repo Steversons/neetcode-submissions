20ccde996f37aaf8054506cce7c07725a02be3ee
Jerry Steve <39701490+Steversons@users.noreply.github.com>
2026-06-23 23:33:50 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty()){
+            return true;
+        }
+
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start < end){
+            char first = s.charAt(start);
+            char last = s.charAt(end);
+            if(!Character.isLetterOrDigit(first)){
+                start++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(last)){
+                end--;
+                continue;
+            }
+            if(Character.toLowerCase(first) != Character.toLowerCase(last)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+        
+    }
+}
